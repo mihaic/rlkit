4a6c4432f498abee4162d3b1441c79ae575479a9
Mihai Capotă <mihai@mihaic.ro>
2019-02-20 15:32:33 -0800

    Add non-exhaustive install_requires

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="rlkit-python",  # Not real name, just placeholder
     packages=find_packages(),
+    install_requires=[  # Not exhaustive
+        "gtimer",
+        "joblib",
+        "torch",
+    ],
 )
